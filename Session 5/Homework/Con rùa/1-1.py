ebd85d4dd2bc686b070bca758a72253dda2a2887
unknown <nhtrung312@gmail.com>
2019-04-16 00:11:24 +0700

    ''

--- Session 5/Homework/Con rùa/1-1.py
+++
@@ -1,4 +0,0 @@
-def xin_chao() :
-    print(" Hello world "*3 )
-    
-xin_chao()
--- Session 5/Homework/Con rùa/1-2.py
+++
@@ -1,8 +0,0 @@
-x = int(input(" nhập số a :"))
-y = int(input('nhập số b :'))
-
-def tinh_tong(a,b) :
-    print(a+b)
-
-
-tinh_tong(x,y)
